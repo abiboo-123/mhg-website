c1d3ade5c865d6098e5ed3d08271a5efb69f45ff
Habib <habubyies@gmail.com>
2025-10-18 19:58:36 +0200

    18-10-2025_06

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
 import react from '@astrojs/react'; // 👈 Add this
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    react(), 
-  ],
+  site: 'https://mhg-website-phi.vercel.app',
+  integrations: [tailwind(), react(), sitemap()],
   output: 'server',
   adapter: vercel(),
 });
